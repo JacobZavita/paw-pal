e56393e4c2982cc9bc0a778d1b82da1049be64f0
Michael Ngo <michaelngo1337@yahoo.com>
2021-07-16 14:57:10 -0700

    put some code in search.js

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,8 +1,55 @@
-const Search = _ => {
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import { useState } from 'react'
+import axios from 'axios'
+import { Button } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch'
+    }
+  }
+}))
+
+const Search = () => {
+  const classes = useStyles()
+
+  const [value, setValue] = useState()
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const handleOnClick = () => {
+    // make axios request here
+    axios({
+      headers: {'Authorization': `Bearer ${process.env.API_TOKEN}`},
+      method: 'get',
+      url: `https://api.petfinder.com/v2/animals?type=${value}&page=1&limit=1`,
+      responseType: 'json'
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
-    <>
-      <h1>This is the Search page</h1>
-    </>
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField
+        id="standard-multiline-flexible"
+        label="Multiline"
+        multiline
+        maxRows={4}
+        value={value}
+        onChange={handleChange}
+      />
+      <Button onClick={handleOnClick} variant="contained" color="primary">
+        Search
+      </Button>
+    </form>
   )
 }
 
