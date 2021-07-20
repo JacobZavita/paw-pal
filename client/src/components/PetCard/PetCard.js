cda6d96c4bec9e9bdc141b35f755bbcd90108d45
Jacob Zavita <jacobzavita@gmail.com>
2021-07-20 14:28:16 -0700

    basic card

--- client/src/components/PetCard/PetCard.js
+++ client/src/components/PetCard/PetCard.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 // import { useState } from 'react'
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography, Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 const useStyles = makeStyles({
   root: {
@@ -20,7 +15,7 @@ const ImgMediaCard = props => {
 
   const testOnClick = () => {
     console.log(props.petState)
-    console.log(props.petState.pets[0])
+    console.log(props.petState.pets[7])
   }
 
   return (
@@ -29,27 +24,48 @@ const ImgMediaCard = props => {
       <CardActionArea>
         <CardMedia
           component="img"
-            alt={props.petState.pets[1].name}
-          height="640"
-            src={props.petState.pets[1].primary_photo_cropped.full}
-          title={props.petState.pets[1].name}
+            alt={props.petState.pets[7].name}
+          // height="640"
+            src={props.petState.pets[7].primary_photo_cropped.full}
+          title={props.petState.pets[7].name}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            {props.petState.pets[1].name}
+            {props.petState.pets[7].name}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Age: {props.petState.pets[1].age}
+            Age: {props.petState.pets[7].age}
           </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Sex: {props.petState.pets[7].gender}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Breed: {props.petState.pets[7].breeds.primary}
+            </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
         <Button size="small" color="primary" onClick={testOnClick}>
-          Learn More
+          Test: see data
         </Button>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          >
+          Learn More About Me
+        </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Email: {props.petState.pets[7].contact.email}
+            </Typography>
+            <br></br>
+            <Typography>
+              Phone: {props.petState.pets[7].contact.phone}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </CardActions>
     </Card>
     </>
--- client/src/pages/Pets/Pets.js
+++ client/src/pages/Pets/Pets.js
@@ -1,22 +1,16 @@
 import ImgMediaCard from '../../components/PetCard'
-import { Button } from '@material-ui/core'
 
 const Pets = props => {
-  
-  const testOnClick = () => {
-    console.log(props.petState.pets)
-  }
 
   return (
-    <div style={{ margin: '75px auto 15px auto' }} align='center'>
-      <ImgMediaCard
-        petState={props.petState}
-        setPetState={props.setPetState}
-      />
-      <Button size="small" color="primary" onClick={testOnClick}>
-        Learn More
-        </Button>
-    </div>
+    <>
+      <div style={{ margin: '75px auto 15px auto' }} align='center'>
+        <ImgMediaCard
+          petState={props.petState}
+          setPetState={props.setPetState}
+        />
+      </div>
+    </>
   )
 }
 
